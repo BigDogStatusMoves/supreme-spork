a8147d82e2da7c6f0642cd361b14637627ea77b2
shawn.kuta23 <shawn.kuta23@d155.org>
2022-08-22 08:32:50 -0500

    code

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,12 @@
+fun main(){
+
+    val x = 10
+    val y = 5
+
+    println("10 + 5 = "+ (x+y))
+    println("10 - 5 = " + (x-y))
+    println("10 * 5 = " + (x*y))
+    println("10 / 5 = " + (x/y))
+    println("10 % 5 = " + (x%y))
+
+}
